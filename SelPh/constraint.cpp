f23beacfa35e7609dfad7851d4e15ec039969b0b
Yuki Koyama <yuki@koyama.xyz>
2019-03-02 00:52:30 +0900

    Remove unused files

--- SelPh/constraint.cpp
+++
@@ -1,9 +0,0 @@
-#include "constraint.h"
-
-Constraint::Constraint(int vertexIndexA, int vertexIndexB, double d) :
-    vertexIndexA(vertexIndexA),
-    vertexIndexB(vertexIndexB),
-    d(d)
-{
-
-}
--- SelPh/vertex.cpp
+++
@@ -1,8 +0,0 @@
-#include "vertex.h"
-
-using namespace std;
-
-Vertex::Vertex(vector<double> x, int index) : x(x), value(0.0), index(index)
-{
-
-}
